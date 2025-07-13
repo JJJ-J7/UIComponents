41b6f3e239408061e204e65e8cc42a68b22d28a2
JJJ-J7 <jojo.reg.services@gmail.com>
2025-07-13 23:20:55 +0900

    main.js を作成

---
+++ main.js
@@ -0,0 +1,40 @@
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: {
+    preload,
+    create
+  }
+};
+
+let game;
+
+function preload() {
+  this.load.image('logo', 'https://labs.phaser.io/assets/sprites/phaser3-logo.png');
+}
+
+function create() {
+  this.logo = this.add.image(this.scale.width / 2, this.scale.height / 2, 'logo');
+
+  // リサイズ時にロゴを中央に再配置
+  this.scale.on('resize', (gameSize) => {
+    const { width, height } = gameSize;
+    this.logo.setPosition(width / 2, height / 2);
+  });
+}
+
+// 初期化とService Worker登録
+window.addEventListener("load", () => {
+  game = new Phaser.Game(config);
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg.scope))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  }
+});
