c0829ac62f4b0a28d6bb94d58ef1263f7ace4fcb
JJJ-J7 <jojo.reg.services@gmail.com>
2025-07-13 13:37:37 +0000

    first

---
+++ sample/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "phaser-pwa-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// インストール時にキャッシュ
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// オフライン対応：キャッシュ優先
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
