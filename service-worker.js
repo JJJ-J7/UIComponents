da176c53147f139a285ca93946f267832654ac9f
JoJo <mars_nonstop7@yahoo.co.jp>
2025-07-19 07:48:27 +0900

    Improve offline support by returning index.html when fetch fails in service worker

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,9 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+          return caches.match("./index.html");
+        });
     })
   );
 });
